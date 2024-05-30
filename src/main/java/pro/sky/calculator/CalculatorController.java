19616037f35a3ccb404580e87ad08c4c19f45261
Ilea <sorokin58473@gmail.com>
2024-05-30 20:28:44 +1000

    Сделал

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,15 +1,36 @@
 package pro.sky.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
-    @GetMapping(path = "/calculator")
+    @GetMapping
     public String hello() {
         return "Добро пожаловать в калькулятор";
     }
 
-    @GetMapping
-    public String
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    @GetMapping(path = "/multiply")
+    public String multiply(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    @GetMapping(path = "/divide")
+    public String divide(@RequestParam double num1, @RequestParam double num2) {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+
+    }
 }
